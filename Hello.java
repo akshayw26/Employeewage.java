b68305e0f21033266b99beef61cb3d9053466847
Akshayw26 <wangade30@gmail.com>
2022-06-28 16:15:38 +0530

    added = Employee Attendance

--- Hello.java
+++ Hello.java
@@ -3,8 +3,17 @@ package javasample1;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employeewage");
-
-     }
-
+		System.out.println("Welcome to Employeewage")
+                int IS_PRESENT=1;
+		
+		double empcheck=Math.floor(Math.random()*10)%2;
+		
+		if(empcheck==IS_PRESENT) {
+			
+			System.out.println("Employee is Present");
+			
+		}else {
+			System.out.println("Employee is absent"); 
+            }
+      }
 }
