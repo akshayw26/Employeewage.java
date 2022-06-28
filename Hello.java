cbbe94fc5d4536b4d7fdbdc51c624b698759c4d5
Akshayw26 <wangade30@gmail.com>
2022-06-28 16:18:33 +0530

    added = Calculate Employeewage

--- Hello.java
+++ Hello.java
@@ -4,7 +4,19 @@ package javasample1;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage");
+                int IS_PRESENT=1;
+		int WagePerHours=20;
+		int WorkingHours=8;
+		
+		double empcheck=Math.floor(Math.random()*10)%2;
+		int empwage=WagePerHours*WorkingHours;
+		if(empcheck==IS_PRESENT) {
+			
+			System.out.println("Employee is Present");
+			System.out.println("Employe wage = " +empwage);
+		}else {
+			System.out.println("Employee is absent"); 
+          }
 
      }
-
 }
