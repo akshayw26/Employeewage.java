f4ca0e0ee8ab19e7aaab44b99e00c916778dd463
Akshayw26 <wangade30@gmail.com>
2022-07-09 09:38:52 +0530

    Added : CAlculating Wage For a Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,14 +4,14 @@ public class EmployeeWage {
 	public static final int Is_Full_Time = 1;
 	public static final int Is_Part_Time = 2;
 	public static final int Emp_Rate_Per_Hours = 20;
+	public static final int Num_of_Working_Days = 20 ;
 	public static void main(String[] args) { 
-
 	//Variables
 	int empHrs = 0;
 	int empWage = 0;
 	int totalEmpWage = 0;
-
 	//Computation
+	for ( int day = 0; day < Num_of_Working_Days; day++ ) {
 	int empCheck =(int)Math.floor(Math.random()*10)%3;
 	switch (empCheck) {
 	 case Is_Part_Time: 
@@ -24,7 +24,10 @@ public class EmployeeWage {
 		empHrs = 0;
 	}
 	empWage = empHrs * Emp_Rate_Per_Hours;
+    totalEmpWage += empWage;
 		System.out.println("Emp Wage: " +empWage);
 	}
+	    System.out.println("Total Emp Wage: " +totalEmpWage);
 
+  }
 }
